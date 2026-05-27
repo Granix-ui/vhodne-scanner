b8f89713605df3d291cc9608eca7a4fdc5b569fb
Granix-ui <Granix@centrum.cz>
2026-05-27 13:46:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,32 +10,31 @@ st.title("Vhodné uveřejnění – aktivní zakázky (okolí Vřesové)")
 urls_text = st.text_area(
     "Zadej URL profilů zadavatelů (jedna na řádek) - seřazeno od nejbližších",
     height=480,
-    value="https://www.vhodne-uverejneni.cz/profil/obec-vresova\n"
-          "https://www.vhodne-uverejneni.cz/profil/00259349\n"          # Těšovice
-          "https://www.vhodne-uverejneni.cz/profil/00254592\n"
-          "https://www.vhodne-uverejneni.cz/profil/00255050\n"
-          "https://www.vhodne-uverejneni.cz/profil/00253936\n"
-          "https://www.vhodne-uverejneni.cz/profil/obec-lomnice?tabs[xenorganization_detail_orders]=1\n"
-          "https://www.vhodne-uverejneni.cz/profil/thermal-f-a-s\n"     # Sokolov
-          "https://www.vhodne-uverejneni.cz/profil/00254819\n"
-          "https://www.vhodne-uverejneni.cz/profil/00573183?tabs[xenorganization_detail_orders]=1\n"
-          "https://www.vhodne-uverejneni.cz/profil/witte-nejdek-spol-s-r-o\n"
-          "https://www.vhodne-uverejneni.cz/profil/00255076\n"
-          "https://www.vhodne-uverejneni.cz/profil/47700521\n"
-          "https://www.vhodne-uverejneni.cz/profil/obec-tesovice-1\n"
-          "https://www.vhodne-uverejneni.cz/profil/26319438\n"
-          "https://www.vhodne-uverejneni.cz/profil/obec-lipova\n"
-          "https://www.vhodne-uverejneni.cz/profil/00259250\n"
-          "https://www.vhodne-uverejneni.cz/profil/obec-cernava\n"
-          "https://www.vhodne-uverejneni.cz/profil/mesto-loket\n"
-          "https://www.vhodne-uverejneni.cz/profil/mesto-plesna\n"
-          "https://www.vhodne-uverejneni.cz/profil/00254231\n"
-          "https://www.vhodne-uverejneni.cz/profil/vodarna-sokolovsko-s-r-o\n"
-          "https://www.vhodne-uverejneni.cz/profil/obec-straz-nad-ohri\n"
-          "# Vzdálenější (30–50 km)\n"
-          "https://www.vhodne-uverejneni.cz/profil/mesto-nova-role\n"
-          "https://www.vhodne-uverejneni.cz/profil/mesto-hroznetin\n"
-          "https://www.vhodne-uverejneni.cz/profil/mesto-tepla"
+    value="https://www.vhodne-uverejneni.cz/profil/obec-vresova\n"          # Vřesová
+          "https://www.vhodne-uverejneni.cz/profil/obec-tesovice\n"          # Těšovice
+          "https://www.vhodne-uverejneni.cz/profil/obec-lomnice\n"          # Lomnice
+          "https://www.vhodne-uverejneni.cz/profil/00254819\n"          # Nová Role
+          "https://www.vhodne-uverejneni.cz/profil/mesto-nova-role\n"          # Nová Role
+          "https://www.vhodne-uverejneni.cz/profil/00259349\n"          # Chodov
+          "https://www.vhodne-uverejneni.cz/profil/00573183?\n"          # Božičany
+          "https://www.vhodne-uverejneni.cz/profil/00259250\n"          # Březová
+          "https://www.vhodne-uverejneni.cz/profil/mesto-loket\n"          # Loket
+          "https://www.vhodne-uverejneni.cz/profil/00254231\n"          # Skalná
+          "https://www.vhodne-uverejneni.cz/profil/26319438\n"          # Chodovské TS
+          "https://www.vhodne-uverejneni.cz/profil/thermal-f-a-s\n"          # Thermal
+          "https://www.vhodne-uverejneni.cz/profil/vodarna-sokolovsko-s-r-o\n"          # Vod.Sokolov
+          "https://www.vhodne-uverejneni.cz/profil/47700521\n"          # VH západní čechy
+          "https://www.vhodne-uverejneni.cz/profil/witte-nejdek-spol-s-r-o\n"          # Witte Nejdek
+          "https://www.vhodne-uverejneni.cz/profil/obec-lipova\n"          # Lipová
+          "https://www.vhodne-uverejneni.cz/profil/obec-cernava\n"          # Černava
+          "https://www.vhodne-uverejneni.cz/profil/mesto-plesna\n"          # Plesná
+          "https://www.vhodne-uverejneni.cz/profil/mesto-hroznetin\n"          # Hroznětín
+          "https://www.vhodne-uverejneni.cz/profil/00254592\n"          # Hroznětín
+          "https://www.vhodne-uverejneni.cz/profil/00255050\n"          # Teplá
+          "https://www.vhodne-uverejneni.cz/profil/mesto-tepla"          # Teplá
+          "https://www.vhodne-uverejneni.cz/profil/00253936\n"          # Fr.Lázně
+          "https://www.vhodne-uverejneni.cz/profil/00255076\n"          # Toužim
+          "https://www.vhodne-uverejneni.cz/profil/obec-straz-nad-ohri\n"          # Stráž nad Ohří
 )
 
 if st.button("Načíst čerstvá data"):
